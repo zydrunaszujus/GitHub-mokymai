2e6edf88d63874de72e734900db7edb16857e724
keistuol1z <skanusmakaronas6@gmail.com>
2021-09-03 19:39:52 +0300

    mano pirmas bandymas

---
+++ Git_hub_antra_uzduotis/Git_hub_antra_uzduotis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Git_hub_antra_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Sarasas = new List<string>();
+            for (int i = 0; i < Sarasas.Count; i++)
+            {
+                Console.WriteLine(Sarasas[i]);
+            }
+        }
+    }
+}
