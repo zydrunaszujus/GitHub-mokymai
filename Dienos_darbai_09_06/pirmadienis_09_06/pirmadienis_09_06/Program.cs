c6b4ad98cd6c240fea27cbf7734f9c8d0d91fb7a
keistuol1z <89936656+zydrunaszujus@users.noreply.github.com>
2021-09-06 19:02:51 +0300

    4

---
+++ Dienos_darbai_09_06/pirmadienis_09_06/pirmadienis_09_06/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pirmadienis_09_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
